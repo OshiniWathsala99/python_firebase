83e8086cb294ccb2a153d4571afcc326a4c88669
OshiniWathsala99 <oshini@3rivetech.com>
2023-10-03 22:08:50 +0530

    working upload image

--- api/crossvalidationapi.py
+++ api/crossvalidationapi.py
@@ -17,7 +17,7 @@ def allowed_file(filename):
     return '.' in filename and filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
 
 @crossvalidationapi.route('/upload/<username>/<disease>', methods=['POST'])
-def upload_image(username, emotion):
+def upload_image(username, disease):
     if 'file' not in request.files:
         return jsonify({'error': 'No file part'})
 
@@ -33,7 +33,7 @@ def upload_image(username, emotion):
         # Store the image URL in Firestore under the specified document ID and emotion
         image_ref = db.collection('validation').document(document_id)
         #image_ref.update({f'{emotion}Url': filename})
-        image_ref.set({f'{emotion}Url': filename})
+        image_ref.set({f'{disease}Url': filename})
 
         # Save the image to the upload folder
         file.save(os.path.join(current_app.config['UPLOAD_FOLDER'], filename))
@@ -42,3 +42,23 @@ def upload_image(username, emotion):
 
     return jsonify({'error': 'Invalid file type'})
 
+@crossvalidationapi.route('/retrieve/<username>/<disease>', methods=['GET'])
+def retrieve_image(username, disease):
+    # Generate the document ID using the username
+    document_id = username.replace(" ", "")
+    image_doc = db.collection('validation').document(document_id).get()
+
+    if not image_doc.exists:
+        return jsonify({'error': 'Image not found for username: ' + username}), 404
+
+    image_url = image_doc.to_dict().get(f'{disease}Url')
+    
+    if not image_url:
+        return jsonify({'error': f'{disease} URL not found for username: ' + username}), 404
+    
+    image_path = os.path.join(current_app.config['UPLOAD_FOLDER'], image_url)
+
+    if not os.path.exists(image_path):
+        return jsonify({'error': f'{disease} image file not found for username: ' + username}), 404
+    
+    return send_file(image_path)
