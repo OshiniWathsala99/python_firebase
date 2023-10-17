99046d9ef44414de4367b403854d8836faff3af9
Oshini_99 <130132146+OshiniWathsala99@users.noreply.github.com>
2023-10-17 14:58:59 +0530

    finalized

--- api/__init__.py
+++ api/__init__.py
@@ -1,11 +1,13 @@
 from flask import Flask
 from firebase_admin import credentials,initialize_app
+from flask_cors import CORS  # Import the CORS extension
 
 cred = credentials.Certificate("C:/Users/TempO/OneDrive/Desktop/python_firebase/api/key.json")
 default_app=initialize_app(cred)
 
 def create_app():
     app=Flask(__name__)
+    #CORS(app) 
     app.config['SECRET_KEY'] = '12344gbh743'
     app.config['UPLOAD_FOLDER'] = 'fimages'
     
