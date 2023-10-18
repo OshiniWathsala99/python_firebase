3d5cfe9e07f2e927b0ba04353fc7833728c0974d
Oshini_99 <130132146+OshiniWathsala99@users.noreply.github.com>
2023-10-18 05:37:23 +0530

    Add multiple image upload

--- api/__init__.py
+++ api/__init__.py
@@ -1,15 +1,19 @@
 from flask import Flask
-from firebase_admin import credentials,initialize_app
+from firebase_admin import credentials,initialize_app,storage
 from flask_cors import CORS  # Import the CORS extension
+import logging
+
 
 cred = credentials.Certificate("C:/Users/TempO/OneDrive/Desktop/python_firebase/api/key.json")
-default_app=initialize_app(cred)
+#firebase_admin.initialize_app(cred, {'storageBucket': 'ecg-diagnosis-system'})
+
+default_app=initialize_app(cred,{'storageBucket': 'ecg-diagnosis-system.appspot.com'})
 
 def create_app():
     app=Flask(__name__)
-    #CORS(app) 
+    CORS(app) 
     app.config['SECRET_KEY'] = '12344gbh743'
-    app.config['UPLOAD_FOLDER'] = 'fimages'
+    app.config['UPLOAD_FOLDER'] = 'resources'
     
     from .userapi import userapi
     from .modelapi import modelapi
@@ -18,6 +22,9 @@ def create_app():
     app.register_blueprint(userapi,url_prefix='/user')
     app.register_blueprint(modelapi,url_prefix='/model')
     app.register_blueprint(crossvalidationapi,url_prefix='/crosscheck')
+    
+    app.logger.setLevel(logging.INFO)  # Set the desired log level
+    app.logger.addHandler(logging.StreamHandler())
     return app
     
     
--- api/crossvalidationapi.py
+++ api/crossvalidationapi.py
@@ -1,64 +1,105 @@
 from flask import Flask, request, jsonify, send_file,Blueprint,current_app
-from firebase_admin import credentials, firestore
+from firebase_admin import credentials, firestore,storage
 from werkzeug.utils import secure_filename
 import os
 
-db=firestore.client()
+
+##################
+# db=firestore.client()
 
 crossvalidationapi=Blueprint('crossvalidationapi',__name__)
 
-# # Define the upload folder
-# UPLOAD_FOLDER = 'fimages'
-ALLOWED_EXTENSIONS = {'jpg', 'jpeg', 'png', 'gif'}
+# # # Define the upload folder
+# # UPLOAD_FOLDER = 'fimages'
+# ALLOWED_EXTENSIONS = {'jpg', 'jpeg', 'png', 'gif'}
 
-# app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
+# # app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
 
-def allowed_file(filename):
-    return '.' in filename and filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
+# def allowed_file(filename):
+#     return '.' in filename and filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
 
-@crossvalidationapi.route('/upload/<username>/<disease>', methods=['POST'])
-def upload_image(username, disease):
-    if 'file' not in request.files:
-        return jsonify({'error': 'No file part'})
+# @crossvalidationapi.route('/upload/<username>/<disease>', methods=['POST'])
+# def upload_image(username, disease):
+#     if 'file' not in request.files:
+#         return jsonify({'error': 'No file part'})
 
-    file = request.files['file']
+#     file = request.files['file']
 
-    if file.filename == '':
-        return jsonify({'error': 'No selected file'})
+#     if file.filename == '':
+#         return jsonify({'error': 'No selected file'})
 
-    if file and allowed_file(file.filename):
-        filename = secure_filename(file.filename)
-        # Generate a unique document ID using the username
-        document_id = username.replace(" ", "")  # Remove spaces from username
-        # Store the image URL in Firestore under the specified document ID and emotion
-        image_ref = db.collection('validation').document(document_id)
-        #image_ref.update({f'{emotion}Url': filename})
-        image_ref.set({f'{disease}Url': filename})
+#     if file and allowed_file(file.filename):
+#         filename = secure_filename(file.filename)
+#         # Generate a unique document ID using the username
+#         document_id = username.replace(" ", "")  # Remove spaces from username
+#         # Store the image URL in Firestore under the specified document ID and emotion
+#         image_ref = db.collection('validation').document(document_id)
+#         #image_ref.update({f'{emotion}Url': filename})
+#         image_ref.set({f'{disease}Url': filename})
 
-        # Save the image to the upload folder
-        file.save(os.path.join(current_app.config['UPLOAD_FOLDER'], filename))
+#         # Save the image to the upload folder
+#         file.save(os.path.join(current_app.config['UPLOAD_FOLDER'], filename))
 
-        return jsonify({'success': 'File uploaded successfully'})
+#         return jsonify({'success': 'File uploaded successfully'})
 
-    return jsonify({'error': 'Invalid file type'})
+#     return jsonify({'error': 'Invalid file type'})
 
-@crossvalidationapi.route('/retrieve/<username>/<disease>', methods=['GET'])
-def retrieve_image(username, disease):
-    # Generate the document ID using the username
-    document_id = username.replace(" ", "")
-    image_doc = db.collection('validation').document(document_id).get()
+# @crossvalidationapi.route('/retrieve/<username>/<disease>', methods=['GET'])
+# def retrieve_image(username, disease):
+#     # Generate the document ID using the username
+#     document_id = username.replace(" ", "")
+#     image_doc = db.collection('validation').document(document_id).get()
 
-    if not image_doc.exists:
-        return jsonify({'error': 'Image not found for username: ' + username}), 404
+#     if not image_doc.exists:
+#         return jsonify({'error': 'Image not found for username: ' + username}), 404
 
-    image_url = image_doc.to_dict().get(f'{disease}Url')
+#     image_url = image_doc.to_dict().get(f'{disease}Url')
     
-    if not image_url:
-        return jsonify({'error': f'{disease} URL not found for username: ' + username}), 404
+#     if not image_url:
+#         return jsonify({'error': f'{disease} URL not found for username: ' + username}), 404
     
-    image_path = os.path.join(current_app.config['UPLOAD_FOLDER'], image_url)
+#     image_path = os.path.join(current_app.config['UPLOAD_FOLDER'], image_url)
 
-    if not os.path.exists(image_path):
-        return jsonify({'error': f'{disease} image file not found for username: ' + username}), 404
+#     if not os.path.exists(image_path):
+#         return jsonify({'error': f'{disease} image file not found for username: ' + username}), 404
     
-    return send_file(image_path)
+#     return send_file(image_path)
+
+
+######
+
+def upload_image_to_firebase(image, folder_name):
+    try:
+        bucket = storage.bucket()
+        blob = bucket.blob(folder_name + '/' + image.filename)
+        blob.upload_from_file(image)  # Use upload_from_file to upload the image
+        return blob.public_url
+    except Exception as e:
+        return str(e)
+
+@crossvalidationapi.route('/upload', methods=['POST'])
+def upload_images():
+    try:
+        # Check if two image files are included in the request
+        if 'file1' not in request.files or 'file2' not in request.files:
+            return jsonify({'message': 'Two image files are required'}), 400
+
+        image1 = request.files['file1']
+        image2 = request.files['file2']
+
+        current_app.logger.info("Attempting to upload image1")
+        # Upload the images to Firebase Storage
+        url1 = upload_image_to_firebase(image1, 'images')
+        url2 = upload_image_to_firebase(image2, 'images')
+        current_app.logger.info("Attempting send rly")
+        if(url1==""):
+            current_app.logger.info("null.....")
+        response_data = {
+            'image1_url': url1,
+            'image2_url': url2,
+            'success': 'File uploaded successfully'
+        }
+
+        return jsonify(response_data), 200
+    except Exception as e:
+        return jsonify({'error': str(e)}), 500
